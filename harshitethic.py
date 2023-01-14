65104311162d87ebc23720ec671e1cc027c6ee09
PiratePennies <103362448+BlockChainBorn@users.noreply.github.com>
2023-01-14 10:30:22 +0100

    Updated with personal keys

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5638262890:AAEBKlYx5QmzzjI4LJTlmvZKa8trJEK_qYA"
 else:
     tgkey = tgenv
 print(tgenv)
